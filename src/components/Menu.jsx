ee374932cbe4b61fe3871c70b28e0127c93afabd
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-06-22 22:55:21 +0900

    feat: add GSAP animations for cocktail image and details transitions in Menu component

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,9 +1,49 @@
 import React, { useRef, useState } from "react";
 import { sliderLists } from "../constants";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Menu = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const contentRef = useRef(null);
+
+  useGSAP(() => {
+    gsap.fromTo("#title", { opacity: 0 }, { opacity: 1, duration: 1 });
+
+    gsap.fromTo(
+      ".cocktail img",
+      { opacity: 0, xPercent: -100 },
+      { opacity: 1, xPercent: 0, duration: 1, ease: "power1.inOut" }
+    );
+
+    gsap.fromTo(
+      ".details h2",
+      {
+        yPercent: 100,
+        opacity: 0,
+      },
+      {
+        yPercent: 0,
+        opacity: 1,
+        duration: 1,
+        ease: "power1.inOut",
+      }
+    );
+
+    gsap.fromTo(
+      ".details p",
+      {
+        yPercent: 100,
+        opacity: 0,
+      },
+      {
+        yPercent: 0,
+        opacity: 1,
+        duration: 1,
+        ease: "power1.inOut",
+      }
+    );
+  }, [currentIndex]);
 
   const totalCocktails = sliderLists.length;
 
