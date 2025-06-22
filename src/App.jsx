69fe3617e1d89232689ee52538ec92b1738d825d
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-06-22 20:33:18 +0900

    feat: implement Navbar component and update App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap/gsap-core";
+import Navbar from "./components/Navbar";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="flex-center h-[100vh]">
-      <div className="text-6xl text-red-500 underline">App</div>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   );
 };
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <a href="#home" className="flex items-center gap-2">
+          <p>Velvet Mojito</p>
+        </a>
+
+        <ul></ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
